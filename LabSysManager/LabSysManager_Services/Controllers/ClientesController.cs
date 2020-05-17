1b6121553d3d1e879fca56f901adfcb183207db7
Florentino Francisco da Silva Neto <netosilva1994@gmail.com>
2020-05-16 22:57:54 -0300

    Criado controller de clientes.

---
+++ LabSysManager/LabSysManager_Services/Controllers/ClientesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LabSysManager_Services.ViewModels;
+using LabSysManager_Services.Services;
+using AutoMapper;
+using LabSysManager_Infra.Context;
+
+namespace LabSysManager_Services.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ClienteAppService clienteAppService;
+        public ClientesController(LabSysManagerContext dbContext, IMapper mapper)
+        {
+            clienteAppService = new ClienteAppService(dbContext, mapper);
+        }
+
+        // GET: api/Clientes/ObterTodosPorIdade/{idade}
+
+        [HttpGet("{idade}")]
+        public async Task<ActionResult<List<ClienteViewModel>>> ObterTodosPorIdade(long idade)
+        {
+            var clientes = await clienteAppService.ObterTodosPorIdade(idade);
+            return clientes;
+        }
+
+        [HttpGet("{estado}")]
+        public async Task<ActionResult<List<ClienteViewModel>>> ObterTodosPorEstado(string estado)
+        {
+            var clientes = await clienteAppService.ObterTodosPorEstado(estado);
+            return clientes;
+        }
+
+        [HttpGet("{cidade}")]
+        public async Task<ActionResult<List<ClienteViewModel>>> ObterTodosPorCidade(string cidade)
+        {
+            var clientes = await clienteAppService.ObterTodosPorCidade(cidade);
+            return clientes;
+        }
+
+        [HttpGet("{estado}")]
+        public async Task<ActionResult<long>> ObterPesoMedioPorEstado(string estado)
+        {
+            var pesoMedio = await clienteAppService.ObterPesoMedioPorEstado(estado);
+            return pesoMedio;
+        }
+
+        [HttpGet("{idade}")]
+        public async Task<ActionResult<long>> ObterPesoMedioPorIdade(long idade)
+        {
+            var pesoMedio = await clienteAppService.ObterPesoMedioPorIdade(idade);
+            return pesoMedio;
+        }
+    }
+}
--- LabSysManager/LabSysManager_Services/Startup.cs
+++ LabSysManager/LabSysManager_Services/Startup.cs
@@ -1,19 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using LabSysManager_Infra.Context;
 using LabSysManager_Services.AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace LabSysManager_Services
 {
@@ -29,7 +22,7 @@ namespace LabSysManager_Services
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Auto Mapper Configurations
+            #region Auto Mapper Configurations
             var mappingConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new DomainToViewModelMappingProfile());
@@ -37,8 +30,9 @@ namespace LabSysManager_Services
 
             IMapper mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
+            #endregion
 
-            services.AddDbContext<LabSysManagerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LabSysManager"))); ;
+            services.AddDbContext<LabSysManagerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LabSysManager")));
             services.AddControllers();
         }
 
