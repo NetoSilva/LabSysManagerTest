342f3fe20940b4335eaa496dc2820faa032cbf85
Florentino Francisco da Silva Neto <netosilva1994@gmail.com>
2020-05-17 00:11:05 -0300

    Adicionado Documentação do Swagger na API.

--- LabSysManager/LabSysManager_Services/Controllers/ClientesController.cs
+++ LabSysManager/LabSysManager_Services/Controllers/ClientesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using LabSysManager_Services.ViewModels;
 using LabSysManager_Services.Services;
 using AutoMapper;
@@ -10,18 +8,27 @@ using LabSysManager_Infra.Context;
 
 namespace LabSysManager_Services.Controllers
 {
+    /// <summary>
+    /// API para exibição dos dados dos Clientes LabSysmanager
+    /// </summary>
     [Route("api/[controller]/[action]")]
     [ApiController]
+
     public class ClientesController : ControllerBase
     {
         private readonly ClienteAppService clienteAppService;
+        /// <summary>
+        /// Construtor
+        /// </summary>
         public ClientesController(LabSysManagerContext dbContext, IMapper mapper)
         {
             clienteAppService = new ClienteAppService(dbContext, mapper);
         }
 
-        // GET: api/Clientes/ObterTodosPorIdade/{idade}
-
+        /// <summary>
+        /// Action para obter todos os clientes por idade.
+        /// </summary>
+        /// <param name="idade">Idade dos clientes</param>
         [HttpGet("{idade}")]
         public async Task<ActionResult<List<ClienteViewModel>>> ObterTodosPorIdade(long idade)
         {
@@ -29,12 +36,22 @@ namespace LabSysManager_Services.Controllers
             return clientes;
         }
 
+        /// <summary>
+        /// Action para obter todos os clientes por estado.
+        /// </summary>
+        /// <param name="estado">UF dos clientes</param>
+     
         [HttpGet("{estado}")]
         public async Task<ActionResult<List<ClienteViewModel>>> ObterTodosPorEstado(string estado)
         {
             var clientes = await clienteAppService.ObterTodosPorEstado(estado);
             return clientes;
         }
+
+        /// <summary>
+        /// Action para obter todos os clientes por cidade..
+        /// </summary>
+        /// <param name="cidade">Cidade dos clientes</param>
 
         [HttpGet("{cidade}")]
         public async Task<ActionResult<List<ClienteViewModel>>> ObterTodosPorCidade(string cidade)
@@ -43,12 +60,22 @@ namespace LabSysManager_Services.Controllers
             return clientes;
         }
 
+        /// <summary>
+        /// Action para obter peso médio de todos os clientes por estado.
+        /// </summary>
+        /// <param name="estado" >UF dos clientes</param>
+
         [HttpGet("{estado}")]
         public async Task<ActionResult<long>> ObterPesoMedioPorEstado(string estado)
         {
             var pesoMedio = await clienteAppService.ObterPesoMedioPorEstado(estado);
             return pesoMedio;
         }
+
+        /// <summary>
+        /// Action para obter peso médio de todos os clientes por idade.
+        /// </summary>
+        /// <param name="idade">Idade dos clientes</param>
 
         [HttpGet("{idade}")]
         public async Task<ActionResult<long>> ObterPesoMedioPorIdade(long idade)
--- LabSysManager/LabSysManager_Services/Startup.cs
+++ LabSysManager/LabSysManager_Services/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace LabSysManager_Services
 {
@@ -32,6 +36,26 @@ namespace LabSysManager_Services
             services.AddSingleton(mapper);
             #endregion
 
+            #region Swagger Configurations
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "API LabSysManager",
+                        Version = "v1",
+                        Description = "API para apresentação de dados dos clientes para Universidade que tem por objetivo avaliar padrões relacionados a obesidade na população."
+                    });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath);
+            });
+            #endregion
+
             services.AddDbContext<LabSysManagerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LabSysManager")));
             services.AddControllers();
         }
@@ -53,6 +77,16 @@ namespace LabSysManager_Services
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                // To deploy on IIS
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LabSysManager API");
             });
         }
     }
